8bd55fe0abbb1b71d19fcd5b212da19a00b232e4
priih <priscila10eloa@gmail.com>
2026-03-09 19:12:33 -0400

    exercicio1

---
+++ ex1.js/pessoa.js
@@ -0,0 +1,10 @@
+class Pessoa {
+  nome;
+  idade;
+}
+
+let pessoa1 = new Pessoa();
+pessoa1.nome = "Maria";
+pessoa1.idade = 30;
+
+console.log("Nome: " + pessoa1.nome + ", Idade: " + pessoa1.idade);
