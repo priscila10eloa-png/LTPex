a74df75b47cca0fae5e1dadfef7b97c1f62fed36
priih <priscila10eloa@gmail.com>
2026-03-11 18:28:44 -0400

    Adiciona exercício 1 com classe Pessoa

---
+++ ex1.js/index.js
@@ -0,0 +1,5 @@
+const Pessoa = require('./pessoa.js');
+
+let pessoa1 = new Pessoa("Maria", 30);
+
+console.log("Nome: " + pessoa1.nome + ", Idade: " + pessoa1.idade);
--- ex1.js/pessoa.js
+++ ex1.js/pessoa.js
@@ -1,10 +1,8 @@
 class Pessoa {
-  nome;
-  idade;
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
 }
 
-let pessoa1 = new Pessoa();
-pessoa1.nome = "Maria";
-pessoa1.idade = 30;
-
-console.log("Nome: " + pessoa1.nome + ", Idade: " + pessoa1.idade);
+module.exports = Pessoa;
