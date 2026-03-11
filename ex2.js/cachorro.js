dc8c349830db1236d8002589ec248b19fe719398
priih <priscila10eloa@gmail.com>
2026-03-11 18:42:20 -0400

    Adiciona exercício 2 com classe Cachorro

--- ex2.js/cachorro.js
+++ ex2.js/cachorro.js
@@ -1,14 +1,12 @@
 class Cachorro {
-  nome;
-  raca;
+  constructor(nome, raca) {
+    this.nome = nome;
+    this.raca = raca;
+  }
 
   latir() {
     console.log("Au au!");
   }
 }
 
-let cachorro1 = new Cachorro();
-cachorro1.nome = "Rex";
-cachorro1.raca = "Vira-lata";
-
-cachorro1.latir();
+module.exports = Cachorro;
---
+++ ex2.js/index.js
@@ -0,0 +1,5 @@
+const Cachorro = require('./cachorro.js');
+
+let cachorro1 = new Cachorro("Rex", "Vira-lata");
+
+cachorro1.latir();
